8b78dc244362ba5761d2970eea27f1290a28bd12
sinaOshrieh <sina.oshrieh@torontomu.ca>
2024-10-09 19:06:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -12,28 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    const profileImage = document.getElementById('profileImage');
-
-    profileImage.addEventListener('click', (event) => {
-        
-        profileImage.classList.toggle('enlarged');
-
-        if (profileImage.classList.contains('enlarged')) {
-            
-            event.stopPropagation();
-
-            document.addEventListener('click', closeEnlargedImage);
-        } else {
-            document.removeEventListener('click', closeEnlargedImage);
-        }
-    });
-
-    function closeEnlargedImage(event) {
-        if (!profileImage.contains(event.target)) {
-            profileImage.classList.remove('enlarged');
-            document.removeEventListener('click', closeEnlargedImage);
-        }
-    }
+    })
 
     const navItems = document.querySelectorAll('.nav-item');
 
@@ -50,11 +29,11 @@ document.addEventListener('DOMContentLoaded', () => {
 });
 function enlargeImage() {
     var img = document.getElementById("profileImage");
-    console.log("dsgdsdgs");
+    
     if (img.style.width === "100%") {
-        img.style.width = "200px"; // Shrink the image back to original size
+        img.style.width = "200px"; 
     } else {
-        img.style.width = "100%"; // Enlarge the image to 100% width of its container
+        img.style.width = "300px"; 
     }
 }
 
